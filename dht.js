ad383011fb2d6f1a993b726784a17b4fc64f7bcc
Sophie Wheeler <me@sophi.ee>
2021-12-24 21:18:49 +0000

    fix cli

--- dht.js
+++ dht.js
@@ -1,12 +1,11 @@
 const sensor = require("node-dht-sensor");
 const cli = require('./cli');
-
 const args = cli.args;
 
 module.exports = { 
     getSensorValue: async function() {
         try {
-            const res = await sensor.read(cli.sensorType, cli.gpio);
+            const res = await sensor.read(args.sensorType, args.gpio);
             return res;
         } catch (err) {
             console.error("Failed to read sensor data:", err);
